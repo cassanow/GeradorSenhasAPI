75784241496fbb1987e4b696d7b3a16162f2b602
Arthur <nashcassano8@gmail.com>
2025-08-07 13:23:57 -0300

    Implementando rate limiter em minha API

--- GeradorSenhasAPI/Program.cs
+++ GeradorSenhasAPI/Program.cs
@@ -1,7 +1,9 @@
 
 
+using System.Threading.RateLimiting;
 using GeradorSenhasAPI.Interface;
 using GeradorSenhasAPI.Service;
+using Microsoft.AspNetCore.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +16,20 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddTransient<ISenhaService, SenhaService>();
 
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("fixed", config =>
+    {
+        config.PermitLimit = 5;
+        config.Window = TimeSpan.FromMinutes(1);
+        config.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        config.QueueLimit = 0;
+    });
+});
+
 var app = builder.Build();
+
+app.UseRateLimiter();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
